b0052d5dd56bf274ab7672da3a3f4926943abc28
seongmin <pok_tan@naver.com>
2024-04-29 00:37:38 +0900

    [Feat] - zustand 도입 - store 생성 - 설비, 파라미터 등 선택하는 창 구현

--- client/src/components/domain/dashboard/OptionSection.jsx
+++ client/src/components/domain/dashboard/OptionSection.jsx
@@ -1,11 +1,28 @@
 import ParameterSelection from "./ParameterSelection";
 import StepSelection from "./StepSelection";
+import DataList from "./DataList";
 function OptionSection() {
 
+
+
+    //     <>
+    //     <div style={{
+    //         display: 'grid',
+    //         gridTemplateColumns: '8fr 4fr',
+    //         padding: '20px'
+    //     }}>
+    //         <GraphSection />
+    //         <OptionSection />
+    //     </div>
+    // </>
     return (
         <div>
-            <ParameterSelection />
-            <StepSelection />
+            <DataList />
+            <div className="flex flex-row ">
+                <ParameterSelection />
+                <StepSelection />
+            </div>
+
         </div>
     )
 }
