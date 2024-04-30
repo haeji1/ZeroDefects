235c42e673840a85066bb3287c14dd0ef84a573e
이예진 <bella_floret@naver.com>
2024-04-30 14:46:49 +0900

    [Fix] 패키지 수정

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,11 +1,14 @@
 import UploadedFileSection from "@/components/domain/settings/UploadedFilesSection";
 
 function Settings() {
-    return (
-        <>
-            <UploadedFileSection></UploadedFileSection>
-        </>
-    )
+  return (
+    <>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl" style={{display: "flex", justifyContent: "center"}}>
+        파일 CRUD
+      </h1>
+      <UploadedFileSection></UploadedFileSection>
+    </>
+  );
 }
 
 export default Settings;
