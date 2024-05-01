5a31ecb5347a50f8fb37f94b12be1f040a2aa48c
혜지 <hms7248@naver.com>
2024-05-01 19:44:54 +0900

    [Feat] draw_dataframe_to_graph 완료

--- server/app/routers/bokehgraph/bokeh_router.py
+++ server/app/routers/bokehgraph/bokeh_router.py
@@ -16,31 +16,7 @@ from fastapi.responses import JSONResponse
 # bokeh
 from bokeh.embed import json_item
 
-file_name = "F1492-ExhaustLog-240323-011325.CSV"
-column = "No1_P[kW]"
-columns = ["No1_P[kW]","No2_P[kW]","No4_P[kW]","No5_P[kW]"
-    ,"No6_P1_Fwd[kW]","No6_P2_Fwd[kW]","No6_P3_Fwd[kW]", "No6_P4_Fwd[kW]"
-    ,"No6_A1[sccm]", "No6_O1[sccm]","No6_O2[sccm]","No6_N1[sccm]"
-    ,"No1_A1[sccm]","No1_A2[sccm]","No1_A3[sccm]","No1_A4[sccm]"
-    ,"No2_A1[sccm]","No2_A2[sccm]","No2_A3[sccm]","No2_A4[sccm]"
-    ,"No4_A1[sccm]","No4_A2[sccm]","No4_A3[sccm]","No4_A4[sccm]"
-    ,"No5_A1[sccm]","No5_A2[sccm]","No5_A3[sccm]","No5_A4[sccm]"]
 
-tg_file_name = "F1508-ExhaustLog-240412-012855.CSV"
-tg_columns = ["P.TG1Pwr[kW]","P.MF211Ar[sccm]","P.MF212Ar[sccm]","P.MF213Ar[sccm]","P.MF214Ar[sccm]"
-    ,"P.TG2Pwr[kW]","P.MF221Ar[sccm]","P.MF222Ar[sccm]","P.MF223Ar[sccm]","P.MF224Ar[sccm]"
-    ,"P.TG4Pwr[kW]","P.MF241Ar[sccm]","P.MF242Ar[sccm]","P.MF243Ar[sccm]","P.MF244Ar[sccm]"
-    ,"P.TG5Pwr[kW]","P.MF251Ar[sccm]","P.MF252Ar[sccm]","P.MF253Ar[sccm]","P.MF254Ar[sccm]"
-    ,"P.Icp1PwrFwd[kW]","P.Icp2PwrFwd[kW]","P.Icp3PwrFwd[kW]","P.Icp4PwrFwd[kW]"
-    ,"P.MF207Ar[sccm]","P.MF208O2[sccm]","P.MF209O2[sccm]","P.MF210N2[sccm]"]
-
-
-df = bokeh_service.load_file(file_name)
-start = 1120
-end = 4948
-
-tg_start = 1026
-tg_end = 5687
 
 router = APIRouter(
     prefix="/bokeh",
@@ -50,19 +26,27 @@ router = APIRouter(
 
 @router.get("/draw/graph")
 async def draw_graph_endpoint():
-    plot = bokeh_service.draw_graph(column, start, end)
-    plot_json = json_item(plot, "my_plot")
-    return JSONResponse(content=plot_json)
+    # plot = bokeh_service.draw_graph(column, start, end)
+    # plot_json = json_item(plot, "my_plot")
+    # return JSONResponse(content=plot_json)
+    return {'results': 'success'}
 
 @router.get("/draw/all-graph")
 async def draw_graph_endpoint():
-    plots = bokeh_service.draw_all_graph(columns, start, end)
-    plot_json = [json_item(plot, f"my_plot_{idx}") for idx, plot in enumerate(plots)]
-    return JSONResponse(content=plot_json)
+    # plots = bokeh_service.draw_all_graph(columns, start, end)
+    # plot_json = [json_item(plot, f"my_plot_{idx}") for idx, plot in enumerate(plots)]
+    # return JSONResponse(content=plot_json)
+    return {'results': 'success'}
 
 @router.get("/draw/all/tg-graph")
 async def draw_graph_endpoint():
-    plots = bokeh_service.draw_all_tg_graph(df, tg_start, tg_end)
+    # plots = bokeh_service.draw_all_tg_graph(df, tg_start, tg_end)
+    # plot_json = [json_item(plot, f"my_plot_{idx}") for idx, plot in enumerate(plots)]
+    # return JSONResponse(content=plot_json)
+    return {'results': 'success'}
+@router.get("/draw/dataframe-to-graph")
+async def draw_graph_endpoint():
+    plots = bokeh_service.dataframe_to_graph(df_list)
     plot_json = [json_item(plot, f"my_plot_{idx}") for idx, plot in enumerate(plots)]
     return JSONResponse(content=plot_json)
 
@@ -91,7 +75,7 @@ def execute_query(client: InfluxDBClient, org: str, query: str) -> List[Dict[str
     for records in result:
         for record in records:
             if flag:
-                iso_time = record.get_time().isoformat() + "Z"
+                iso_time = record.get_time().isoformat() # + "Z"
                 factor_dictionary['Time'].append(iso_time)
             factor_dictionary[record.get_field()].append(record.get_value())
         flag = False
@@ -136,4 +120,11 @@ async def read_influxdb(conditions: List[FacilityData]):
     print('df_list', df_list)
     # facility_list, parameter_list, df_list를 활용해서 bokeh 그리고 return 하면돼
 
-    return {'result': results}
+    plots = bokeh_service.draw_dataframe_to_graph(df_list)
+    print("==================plot====================")
+    print(plots)
+    plot_json = [json_item(plot, f"my_plot_{idx}") for idx, plot in enumerate(plots)]
+    print("=========plot_json==========")
+    print(plot_json)
+    return JSONResponse(content=plot_json)
+    # return {'result': results}
