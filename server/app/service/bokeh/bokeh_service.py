08a886d4b44352827467e709f55828accfbb5c7b
혜지 <hms7248@naver.com>
2024-05-03 22:01:11 +0900

    [Feat] 지점별 정보 표시

--- server/app/service/bokeh/bokeh_service.py
+++ server/app/service/bokeh/bokeh_service.py
@@ -1,153 +1,80 @@
 # bokeh
-from bokeh.models import ColumnDataSource, DatetimeTickFormatter, BasicTickFormatter, DatetimeTicker,SingleIntervalTicker
+from bokeh.models import (ColumnDataSource, DatetimeTickFormatter, HoverTool)
+from bokeh.models.formatters import NumeralTickFormatter
 from bokeh.palettes import Category10_10
 from bokeh.plotting import figure
 
 # data frame
 import pandas as pd
 
-# def draw_dataframe_to_graph(df_list,facility_list):
-#
-#     # 데이터 프레임 값 계산하는 함수 호출
-#     calc_df_values(df_list)
-#
-#     if len(df_list) == 1:  # 데이터프레임이 하나일 때
-#         return draw_single_dataframe_to_graph(df_list[0])
-#     else:  # 데이터프레임이 여러 개일 때
-#         plots = []
-#
-#         # figure 객체 생성
-#         p = figure(title="Facility Comparison", sizing_mode="stretch_width", x_axis_label='Time (seconds)', y_axis_label='Value',
-#                     height=400)
-#
-#         # 색상 리스트
-#         colors = Category10_10
-#
-#         for i, df in enumerate(df_list):
-#             # 가장 빠른 시작 시간 추출
-#             start_time = pd.to_datetime(df["Time"].str.replace("Z", ""), utc=True).min()
-#
-#             df_name = facility_list[i]
-#
-#             df["Time"] = (pd.to_datetime(df["Time"].str.replace("Z", ""), utc=True) - start_time)
-#
-#             # 색상 선택
-#             color = colors[i % len(colors)]
-#
-#             # ColumnDataSource 생성
-#             source = ColumnDataSource(data=df)
-#
-#             # 각 컬럼 데이터를 그래프에 추가하면서 다른 색상 지정
-#             for j, column_name in enumerate(df.columns[1:]):
-#                 p.line(x='Time', y=column_name, source=source, legend_label=f"{df_name} - {column_name}", color=color)
-#
-#         p.xaxis.formatter = DatetimeTickFormatter(seconds='%S')
-#         # 범례 표시
-#         p.legend.location = "top_left"
-#
-#         plots.append(p)  # 리스트에 그래프 추가
-#
-#         return plots  # 그래프 리스트 반환
-
-from bokeh.models.formatters import NumeralTickFormatter
 
 def draw_dataframe_to_graph(df_list, facility_list):
 
-    # 데이터 프레임 값 계산하는 함수 호출
-    calc_df_values(df_list)
-
-    if len(df_list) == 1:  # 데이터프레임이 하나일 때
+    if len(df_list) == 1:
         return draw_single_dataframe_to_graph(df_list[0])
-    else:  # 데이터프레임이 여러 개일 때
+    else:
         plots = []
-
-        # figure 객체 생성
         p = figure(title="Facility Comparison", sizing_mode="stretch_width", x_axis_label='Time (seconds)',
                    y_axis_label='Value',
                    height=400)
 
-        # 색상 리스트
         colors = Category10_10
 
         for i, df in enumerate(df_list):
-            # 가장 빠른 시작 시간 추출
+
             start_time = pd.to_datetime(df["Time"].str.replace("Z", ""), utc=True).min()
-
             df_name = facility_list[i]
-
             df["Time"] = (pd.to_datetime(df["Time"].str.replace("Z", ""), utc=True) - start_time).dt.total_seconds()
-
-            # 색상 선택
             color = colors[i % len(colors)]
-
-            # ColumnDataSource 생성
             source = ColumnDataSource(data=df)
 
-            # 각 컬럼 데이터를 그래프에 추가하면서 다른 색상 지정
+
             for j, column_name in enumerate(df.columns[1:]):
-                p.line(x='Time', y=column_name, source=source, legend_label=f"{df_name} - {column_name}",
+                line = p.line(x='Time', y=column_name, source=source, legend_label=f"{df_name} - {column_name}",
                        color=color)
 
-        # 시작 시간을 0으로 설정
+                hover = HoverTool(renderers=[line], tooltips=[
+                    ('facility', f'{df_name}'),
+                    ('time', '@Time seconds'),
+                    ('Value', '$y')
+                ])
+
+            p.add_tools(hover)
+
         p.x_range.start = 0
-
-        # x 축의 시간 형식을 초로 설정
         p.xaxis.formatter = NumeralTickFormatter(format="0")
-
-        # 범례 표시
         p.legend.location = "top_left"
+        plots.append(p)
 
-        plots.append(p)  # 리스트에 그래프 추가
-
-        return plots  # 그래프 리스트 반환
-
+        return plots
 
 
 def draw_single_dataframe_to_graph(df):
-    plots = []
 
-    # 전체 데이터를 담을 빈 리스트 생성
+    plots = []
     all_data = []
 
-    # "Z" 제거
     df["Time"] = df["Time"].str.replace("Z", "")
-
-    # pd.to_datetime을 사용하여 변환
     time_values = pd.to_datetime(df["Time"], utc=True)
 
-    # 각 컬럼 데이터를 하나의 ColumnDataSource로 결합
     combined_data = dict(x=time_values)
-
     for column in df.columns:
         if column != "Time":
             column_data = df[column]
             all_data.append(column_data)
             combined_data[column] = column_data.values
 
-    # ColumnDataSource 생성
     source = ColumnDataSource(data=combined_data)
 
-    # figure 객체 생성
     p = figure(title="facility", x_axis_label='Time', y_axis_label='Value',
                width=1200, height=400)
 
-    # 각 컬럼 데이터를 그래프에 추가하면서 다른 색상 지정
-    for i, column_name in enumerate(df.columns[1:]):  # 첫 번째 컬럼은 'Time'이므로 제외합니다
+    for i, column_name in enumerate(df.columns[1:]):
         p.line(x='x', y=column_name, source=source, legend_label=column_name, color=Category10_10[i])
 
     p.xaxis.formatter = DatetimeTickFormatter(hours='%H:%M:%S')
-    # 범례 표시
     p.legend.location = "top_left"
-
     plots.append(p)
 
     return plots
 
-# 사이클에 대한 max, min, 평균 값 구하기
-def calc_df_values(df_list):
-
-    print("========== df 테스트 ===========")
-    print(df_list)
-    for df in df_list:
-        df_columns = df.columns.tolist()
-        print(df_columns)
