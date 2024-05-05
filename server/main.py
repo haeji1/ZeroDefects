13edb4d7d6288c5b65b67422063e2d8a5c445542
안윤철 <anssd1@naver.com>
2024-05-05 16:37:03 +0900

    [TEST] timescale db test

--- server/main.py
+++ server/main.py
@@ -6,6 +6,8 @@ from app.dbtest import timescale
 # bokeh
 
 from app.models.influx.influx_models import FacilityData
+# section
+from app.utils.functions.section import get_section_data
 
 from app.routers.bokeh import bokeh_router
 from app.routers.influx import influx_router
@@ -36,3 +38,4 @@ app.include_router(bokeh_router.router)
 app.include_router(influx_router.influx_router)
 app.include_router(mongo_router.mongo_router)
 app.include_router(section_router.section_router)
+app.include_router(timescale.timescale_router)
