2205f35cd469b20599e21f5bd3c5c3571dade2db
이예진 <bella_floret@naver.com>
2024-05-08 13:51:07 +0900

    [Refact] facility 어제 업데이트 된 거 수정

--- server/app/domain/facility/repository/facility_repository.py
+++ server/app/domain/facility/repository/facility_repository.py
@@ -4,7 +4,7 @@ import time
 from io import StringIO
 
 import pandas as pd
-from fastapi import UploadFile, File
+from fastapi import UploadFile, File, HTTPException
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS, WriteOptions, ASYNCHRONOUS, PointSettings
 from influxdb_client.client.write.dataframe_serializer import data_frame_to_list_of_points
@@ -13,6 +13,8 @@ from typing import List, Any
 from loguru import logger
 from starlette.responses import JSONResponse
 
+from app.domain.facility.service.facility_query import field_time_query, execute_query
+from app.domain.section.model.section_data import SectionData
 from config import settings
 
 # from influxapi.schemas import InfluxWaveRecord
@@ -53,7 +55,7 @@ class FacilityRepository:
         self.client = InfluxDBClient(url=url, token=token, org=org, timeout=60000)
 
     # write
-    async def write_csv(self, files: List[UploadFile] = File(...), batch_size=900) -> JSONResponse:
+    def write_csv(self, files: List[UploadFile] = File(...), batch_size=900) -> []:
         total_time = time.time()
         # create write api
         write_api = self.client.write_api(write_options=
@@ -77,50 +79,88 @@ class FacilityRepository:
             print('time: ', time.time() - start_time)
             print('-----------------------')
 
-
         write_api.close()
         print('total time: ', time.time() - total_time)
-        return JSONResponse(status_code=200, content=response)
+        return response
 
-    @classmethod
-    def write_df(cls, write_api, file: File(), batch_size=1000):
-        print('write csv', file.filename)
+    def read_data(self, conditions: List[SectionData]) -> []:
+        start_time = time.time()
+        result_df = pd.DataFrame()
 
-        # check file format
-        if not file.content_type == 'text/csv':
-            return {"filename": file.filename, "message": "Invalid CSV file"}
-        # check file name
-        if len(file.filename.split('-')) < 3:
-            return {"filename": file.filename, "message": "Invalid file name"}
+        # conditions length == 1
+        if len(conditions) == 1:
+            query = field_time_query(
+                b=self.bucket_name, facility=conditions[0].facility, field=conditions[0].parameter,
+                start_date=conditions[0].startTime, end_date=conditions[0].endTime)
+            try:
+                result_df = execute_query(self.client, query)
+                result_df.rename(
+                    columns={f'{conditions[0].parameter}': f'{conditions[0].facility}_{conditions[0].parameter}'},
+                    inplace=True)
+            except Exception as e:
+                raise HTTPException(500, str(e))
 
-        measurement = file.filename.rsplit('-')[0]
-        date_string = file.filename.rsplit('-')[2]
-        ymd_string = f"20{date_string[:2]}-{date_string[2:4]}-{date_string[4:]} "
+        # conditions length > 1
+        if len(conditions) > 1:
+            for condition in conditions:
+                query = field_time_query(
+                    b=self.bucket_name, facility=condition.facility, field=condition.parameter,
+                    start_date=condition.startTime, end_date=condition.endTime)
 
-        try:
-            df = pd.read_csv(file.file)
+                try:
+                    result_df['Time'] = execute_query(self.client, query)[['Time']]
+                except Exception as e:
+                    print(e)
 
-            df['TempTime'] = pd.to_datetime(ymd_string + df['Time'])
-            df['shift'] = (df['Time'] < df['Time'].shift(1)).cumsum()
-            df['DateTime'] = df.apply(lambda x: x['TempTime'] + pd.DateOffset(days=x['shift']), axis=1)
+                try:
+                    result_df[f'{condition.facility}_{condition.parameter}'] = execute_query(self.client, query)[
+                        [condition.parameter]]
+                    # df_list.append(df)
+                except Exception as e:
+                    raise HTTPException(500, str(e))
 
-            save_section_data(measurement, df[['DateTime', 'RcpReq[]', 'CoatingLayerN[Layers]']])
-            # print("write_df:", df[['DateTime', 'RcpReq[]', 'CoatingLayerN[Layers]']])
+        print('time: ', time.time() - start_time)
+        return ["step", result_df]
 
-            df_modified = df.drop(columns=['Time', 'TempTime', 'shift'])
-            float_cols = df_modified.columns.drop('DateTime')
-            df_modified[float_cols] = df_modified[float_cols].astype(float)
-
-            data = data_frame_to_list_of_points(data_frame=df_modified,
-                                                data_frame_measurement_name=measurement,
-                                                data_frame_timestamp_column='DateTime',
-                                                point_settings=PointSettings())
-
-            write_api.write(bucket=settings.influx_bucket, record=data)
-            return {"filename": file.filename, "message": "file successfully written"}
-        except Exception as e:
-            logger.error(f"Error fetching item : {e}", exc_info=True)
-            return {"filename": file.filename, "message": str(e)}
+    # @classmethod
+    # def write_df(cls, write_api, file: File(), batch_size=1000):
+    #     print('write csv', file.filename)
+    #
+    #     # check file format
+    #     if not file.content_type == 'text/csv':
+    #         return {"filename": file.filename, "message": "Invalid CSV file"}
+    #     # check file name
+    #     if len(file.filename.split('-')) < 3:
+    #         return {"filename": file.filename, "message": "Invalid file name"}
+    #
+    #     measurement = file.filename.rsplit('-')[0]
+    #     date_string = file.filename.rsplit('-')[2]
+    #     ymd_string = f"20{date_string[:2]}-{date_string[2:4]}-{date_string[4:]} "
+    #
+    #     try:
+    #         df = pd.read_csv(file.file)
+    #
+    #         df['TempTime'] = pd.to_datetime(ymd_string + df['Time'])
+    #         df['shift'] = (df['Time'] < df['Time'].shift(1)).cumsum()
+    #         df['DateTime'] = df.apply(lambda x: x['TempTime'] + pd.DateOffset(days=x['shift']), axis=1)
+    #
+    #         save_section_data(measurement, df[['DateTime', 'RcpReq[]', 'CoatingLayerN[Layers]']])
+    #         # print("write_df:", df[['DateTime', 'RcpReq[]', 'CoatingLayerN[Layers]']])
+    #
+    #         df_modified = df.drop(columns=['Time', 'TempTime', 'shift'])
+    #         float_cols = df_modified.columns.drop('DateTime')
+    #         df_modified[float_cols] = df_modified[float_cols].astype(float)
+    #
+    #         data = data_frame_to_list_of_points(data_frame=df_modified,
+    #                                             data_frame_measurement_name=measurement,
+    #                                             data_frame_timestamp_column='DateTime',
+    #                                             point_settings=PointSettings())
+    #
+    #         write_api.write(bucket=settings.influx_bucket, record=data)
+    #         return {"filename": file.filename, "message": "file successfully written"}
+    #     except Exception as e:
+    #         logger.error(f"Error fetching item : {e}", exc_info=True)
+    #         return {"filename": file.filename, "message": str(e)}
 
     @classmethod  # 현재 사용 안함
     def write_df(cls, write_api, file: File(), batch_size=1000):
@@ -215,3 +255,26 @@ class FacilityRepository:
         buckets = bucket_api.find_buckets().buckets
         if not any(b.name == settings.influx_bucket for b in buckets):
             bucket_api.create_bucket(bucket_name=settings.influx_bucket, org=settings.influx_org)
+
+
+    @classmethod
+    def get_measurement_code(cls, input):
+        mapping = {
+            'F1492': 'MASS01',
+            'F1494': 'MASS02',
+            'F1495': 'MASS04',
+            'F1493': 'MASS05',
+            'F1500': 'MASS06',
+            'F1496': 'MASS07',
+            'F1497': 'MASS08',
+            'F1498': 'MASS09',
+            'F1502': 'MASS10',
+            'F1503': 'MASS11',
+            'F1501': 'MASS12',
+            'F1504': 'MASS13',
+            'F1505': 'MASS14',
+            'F1507': 'MASS18',
+            'F1508': 'MASS22',
+        }
+
+        return mapping.get(input, "unknown")
