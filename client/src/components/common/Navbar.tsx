f3c7ae2b7c95d526ffdb6bd7cbe6883f3da4cc33
seongmin <pok_tan@naver.com>
2024-05-08 21:10:12 +0900

    [Refactor] Navbar.tsx 타입스크립트로 리팩토링

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import SamsungLogo from '@/assets/images/Logo_WHITE.png';
 import { Link, Outlet } from 'react-router-dom';
 import './Navbar.css'
@@ -9,13 +9,14 @@ function Navbar() {
 
     const [activeLink, setActiveLink] = useState('dashboard');
 
-    const handleClick = (name) => {
-        setActiveLink(name);
-    };
-
-    const selectedLinkClass = (name) => {
+    const selectedLinkClass = (name : string) => {
         return activeLink === name ? 'selected' : '';
     };
+
+    const handleClickLink = (e : React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+        const {id} = e.target as React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>;
+       setActiveLink(id!);
+    }
 
     return (
         <>
@@ -24,17 +25,22 @@ function Navbar() {
                     <img src={SamsungLogo} className="h-full" alt="Samsung" />
                 </a>
                 <div className='flex flex-row space-x-8 text-[25px] items-center mx-10'>
-                    <Link to="/dashboard" onClick={() => handleClick('dashboard')}>
-                        <p className={`link ${selectedLinkClass('dashboard')}`}>대시보드</p>
+                    <Link to="/dashboard">
+                        <p id='dashboard' className={`link ${selectedLinkClass('dashboard')}`} onClick={handleClickLink}>
+                            대시보드
+                        </p>
                     </Link>
-                    <Link to="/analysis" onClick={() => handleClick('analysis')}>
-                        <p className={`link ${selectedLinkClass('analysis')}`}>분석</p>
+                    <Link to="/analysis">
+                        <p id='analysis' className={`link ${selectedLinkClass('analysis')}`} onClick={handleClickLink}
+                        >분석</p>
                     </Link>
-                    <Link to="/notification" onClick={() => handleClick('notification')}>
-                        <p className={`link ${selectedLinkClass('notification')}`}>알림</p>
+                    <Link to="/notification">
+                        <p id='notification' className={`link ${selectedLinkClass('notification')}`} onClick={handleClickLink}>
+                            알림
+                            </p>
                     </Link>
-                    <Link to="/settings" onClick={() => handleClick('settings')}>
-                        <p className={`link ${selectedLinkClass('settings')}`}>관리</p>
+                    <Link to="/settings">
+                        <p id='settings' className={`link ${selectedLinkClass('settings')}`} onClick={handleClickLink}>관리</p>
                     </Link>
                 </div>
             </div>
