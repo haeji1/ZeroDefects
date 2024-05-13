d691d74d0af49c1ec503858e554a5553cf4abed7
이예진 <bella_floret@naver.com>
2024-05-13 15:11:20 +0900

    [Feat] add update and delete

--- server/app/domain/board/controller/board_controller.py
+++ server/app/domain/board/controller/board_controller.py
@@ -1,68 +1,34 @@
 from typing import List
 
 from fastapi import HTTPException, APIRouter
-from fastapi.encoders import jsonable_encoder
-from pymongo import MongoClient
-from fastapi.responses import JSONResponse
-import pandas as pd
-from starlette import status
 
-from app.domain.board.model.board import Post, Comment
-from app.domain.board.service.board_service import incremental_id
-from app.domain.facility.service.facility_function import get_measurement_code
-from config import settings
+from app.domain.board.model.board import Post
+from app.domain.board.service.board_service import update_post_from_db, \
+    delete_post_from_db, read_post_from_db, get_post_from_db, create_post_from_db
 
 post_router = APIRouter(prefix="/post", tags=['post'])
-
-url = settings.mongo_furl
-
-# MongoDB client
-client = MongoClient(url)
-# database name is setting
-db = client["post"]
 
 
 @post_router.post("/posts")
 async def create_post(post: Post):
-    id_list = db.posts.find({}, sort=[("id", -1)]).limit(1)
-    list_id = 0
-    id_list = list(id_list)  # 커서를 리스트로 변환
-    if list(id_list):
-        list_id = incremental_id(id_list[0]["id"])
-    post.id = list_id
-    print(post)
-    new_post = db.posts.insert_one(post.dict())
-
-    if new_post.inserted_id:
-        return {"message": "Post created successfully."}  # JSONResponse 대신 직접 반환
-    else:
-        return JSONResponse(content={"message": "Failed to create post."}, status_code=status.HTTP_400_BAD_REQUEST)
+    return create_post_from_db(post)
 
 
 @post_router.get("/posts", response_model=List[Post])
 async def get_posts():
-    posts = db.posts.find()
-    a = db.posts.find().sort("id", -1).limit(1)
-    print(list(a))
-    return posts
+    return get_post_from_db()
 
 
-# 특정 게시글 조회 API
 @post_router.get("/posts/{post_id}")
 async def read_post(post_id: int):
-    post = db.posts.find_one({"id": post_id})
+    return read_post_from_db(post_id)
 
-    if post is None:
-        return JSONResponse(status_code=404, content={"detail": "Post not found"})
 
-    post_data = {
-        "id": post["id"],
-        "title": post["title"],
-        "content": post["content"],
-        "nickname": post["nickname"],
-        "password": post["password"],
-        "comments": [Comment(**comment) for comment in post.get("comments", [])]
-    }
-    post2 = Post(**post_data)
+@post_router.delete("/posts/{post_id}")
+async def delete_post(post_id: int):
+    return delete_post_from_db(post_id)
 
-    return JSONResponse(status_code=200, content=post2.dict())
+
+@post_router.put("/posts/{post_id}")
+async def update_post(post_id: int, post: Post):
+    return update_post_from_db(post_id, post)
--- server/app/domain/board/service/board_service.py
+++ server/app/domain/board/service/board_service.py
@@ -1,35 +1,80 @@
-# import os
-#
-# from fastapi import Depends, HTTPException
-# from fastapi.security import HTTPBasic, HTTPBasicCredentials
-# from pymongo import MongoClient
-# from passlib.context import CryptContext
-#
-# from app.domain.board.model.board import Post
-#
-# url = os.getenv('MONGO_FURL')
-# client = MongoClient(url)
-# db = client["board"]
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-# security = HTTPBasic()
-#
-#
-# def verify_password(plain_password, hashed_password):
-#     return pwd_context.verify(plain_password, hashed_password)
-#
-#
-# def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-#     user = db.posts.find_one({"author": credentials.username})
-#     if user and verify_password(credentials.password, user['password']):
-#         return credentials.username
-#     raise HTTPException(status_code=400, detail="Incorrect email or password")
-#
-#
-# async def create_post(post: Post):
-#     post_dict = post.dict()
-#     post_dict['password'] = pwd_context.hash(post_dict['password'])
-#     db.posts.insert_one(post_dict)
-#     return {"message": "Post created successfully"}
+from pymongo import MongoClient
+from app.domain.board.model.board import Post, Comment
+from config import settings
+from fastapi.responses import JSONResponse
+
+url = settings.mongo_furl
+
+# MongoDB client
+client = MongoClient(url)
+# database name is setting
+db = client["post"]
+
 
 def incremental_id(id):
     return id + 1
+
+
+def post_to_post2(post: Post):
+    post_data = {
+        "id": post["id"],
+        "title": post["title"],
+        "content": post["content"],
+        "nickname": post["nickname"],
+        "password": post["password"],
+        "comments": [Comment(**comment) for comment in post.get("comments", [])]
+    }
+    post2 = Post(**post_data)
+    return post2
+
+
+def update_post_from_db(post_id: int, post: Post):
+    changing_post = db.posts.find_one({"id": post_id})
+
+    if changing_post is None:
+        return JSONResponse(status_code=404, content={"detail": "Post not found"})
+
+    update_data = post.dict(exclude_unset=True)
+    print(update_data)
+    db.posts.update_one({"id": post_id}, {"$set": update_data})
+    return JSONResponse(status_code=200, content={"message": "Post updated successfully."})
+
+
+def delete_post_from_db(post_id: int):
+    post = db.posts.find_one({"id": post_id})
+    if post is None:
+        return JSONResponse(status_code=404, content={"detail": "Post not found"})
+
+    db.posts.delete_one({"id": post_id})
+    return JSONResponse(status_code=200, content={"message": "Post deleted successfully."})
+
+
+def read_post_from_db(post_id: int):
+    post = db.posts.find_one({"id": post_id})
+
+    if post is None:
+        return JSONResponse(status_code=404, content={"detail": "Post not found"})
+
+    post2 = post_to_post2(post)
+
+    return JSONResponse(status_code=200, content=post2.dict())
+
+
+def get_post_from_db():
+    posts = db.posts.find()
+    return posts
+
+
+def create_post_from_db(post: Post):
+    id_list = db.posts.find({}, sort=[("id", -1)]).limit(1)
+    list_id = 0
+    id_list = list(id_list)  # 커서를 리스트로 변환
+    if id_list:
+        list_id = incremental_id(id_list[0]["id"])
+    post.id = list_id
+    new_post = db.posts.insert_one(post.dict())
+
+    if new_post.inserted_id:
+        return {"message": "Post created successfully."}  # JSONResponse 대신 직접 반환
+    else:
+        return JSONResponse(content={"message": "Failed to create post."}, status_code=status.HTTP_400_BAD_REQUEST)
