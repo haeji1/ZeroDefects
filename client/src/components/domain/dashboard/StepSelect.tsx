8b9d038348a45dad01ef1c0c2567ae589169b566
seongmin <pok_tan@naver.com>
2024-05-16 10:48:08 +0900

    [Feat] 그래프 조회 조건 오류날 수 있는 항목들 수정 - 시작 시간이 종료 시간 이후이면 조회 버튼 활성화되지 않음 - 선택된 설비 항목들 없으면 그래프 활성화되지 않음

--- client/src/components/domain/dashboard/StepSelect.tsx
+++ client/src/components/domain/dashboard/StepSelect.tsx
@@ -1,12 +1,12 @@
 import { Label } from "@/components/base/label";
 import { Input } from "@/components/base/input";
-import { useQueryStepStore, useQueryButtonStore } from "@/stores/QueryCondition";
+import { useQueryStepStore, useQueryTypeStore } from "@/stores/QueryCondition";
 import { useEffect } from "react";
 
 export default function StepSelect() {
 
-    const { queryStartStep, setQueryStartStep, queryEndStep, setQueryEndStep, stepValid, setStepValid } = useQueryStepStore();
-    const { queryTypeButton } = useQueryButtonStore();
+    const { queryStartStep, setQueryStartStep, queryEndStep, setQueryEndStep, setStepValid } = useQueryStepStore();
+    const { queryType } = useQueryTypeStore();
 
     // 숫자 이외의 입력 값들을 제거하고, Number 타입으로 변환하여 전역값으로 저장
     const handleStepChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,11 +26,11 @@ export default function StepSelect() {
             <div className="flex flex-row gap-3">
                 <div className="w-full">
                     <Label>시작 스텝</Label>
-                    <Input id="startStep" type="number" min={0} value={queryStartStep} onChange={handleStepChange} disabled={queryTypeButton !== "step"} />
+                    <Input id="startStep" type="number" min={0} value={queryStartStep} onChange={handleStepChange} disabled={queryType !== "step"} />
                 </div>
                 <div className="w-full">
                     <Label>종료 스텝</Label>
-                    <Input id="endStep" type="number" min={0} value={queryEndStep} onChange={handleStepChange} disabled={queryTypeButton !== "step"} />
+                    <Input id="endStep" type="number" min={0} value={queryEndStep} onChange={handleStepChange} disabled={queryType !== "step"} />
                 </div>
             </div>
             {queryStartStep - queryEndStep > 0 ?
