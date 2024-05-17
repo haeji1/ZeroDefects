5fcd51cea04105845d566660955732c690a657f0
seongmin <pok_tan@naver.com>
2024-05-17 11:04:58 +0900

    [Feat] 상관 분석 기본 프레임 구성

--- client/src/pages/Correlation.tsx
+++ client/src/pages/Correlation.tsx
@@ -1,8 +1,29 @@
+import { Sidebar } from 'react-pro-sidebar';
+import GraphSection from '@/components/domain/correlation/GraphSection';
+import GetGraph from '@/components/domain/correlation/GetGraph';
+import CorrelationTable from '@/components/domain/correlation/CorrelationTable';
+
+
+
 function Correlation() {
+
+    let isCollapse = false;
+
     return (
-        <>
-            상관 분석 페이지입니다.
-        </>
+        <div className="flex flex-row-reverse m-5">
+            <Sidebar
+                collapsed={isCollapse}
+                width="40%"
+                collapsedWidth="0%"
+                backgroundColor="null"
+            >
+                <CorrelationTable />
+                <GetGraph />
+            </Sidebar>
+            <div className="w-full">
+                <GraphSection />
+            </div>
+        </div>
     )
 }
 
